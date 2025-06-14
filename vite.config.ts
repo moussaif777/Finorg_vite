5f3b3cb56ded13c437867f568715e41754bda396
moussaif777 <mohammed1028@hotmail.com>
2025-06-14 15:30:45 +0400

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    // Skip TypeScript checking in production
+    drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
+  }
 })
